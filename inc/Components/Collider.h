fcd2ec097fece96ea0dcfc4b65499bd9c74f5144
Tarook <Tarook.Zinou@gmail.com>
2025-04-28 07:28:16 -0400

    Feat: Add destroy function to lifecycle

--- inc/Components/Collider.h
+++ inc/Components/Collider.h
@@ -7,23 +7,34 @@
 #include <memory>
 
 #include "../../cmake-build-debug/_deps/bullet-src/src/BulletCollision/CollisionShapes/btCollisionShape.h"
+#include "BulletCollision/CollisionShapes/btBoxShape.h"
+#include "BulletCollision/CollisionShapes/btSphereShape.h"
 
 class Collider : public BaseComponent {
 public:
     explicit Collider(btCollisionShape* shape, const std::shared_ptr<GameObject> &gameObject)
         : BaseComponent(gameObject), _collisionShape(shape) {}
 
-    btCollisionShape* collisionShape() const { return _collisionShape; }
+    btCollisionShape* collisionShape() const { return _collisionShape; } // this is a reference to the shape, there is no need to instantiate a shape every time
 
     void serialize(std::ostream& os) const override {
-        // Serialize Collider-specific data (e.g., collision shape type and parameters)
+        // Serialize Collider-specific data
         // Placeholder: You need to implement serialization for the collision shape
+        os.write(reinterpret_cast<const char*>(&_collisionShape), sizeof(_collisionShape));
     }
 
     void deserialize(std::istream& is) override {
         // Deserialize Collider-specific data
         // Placeholder: You need to implement deserialization for the collision shape
+        is.read(reinterpret_cast<char*>(&_collisionShape), sizeof(_collisionShape));
     }
+
+    void destroy() override {
+        if (_collisionShape) {
+            _collisionShape = nullptr;
+        }
+    }
+
 
 private:
     btCollisionShape* _collisionShape;
