b431a370568496bfd8e036a9c9d12bdb63762da8
Tarook <Tarook.Zinou@gmail.com>
2025-04-29 04:22:27 -0400

    Feat: World runs on server

--- src/DedicatedServerMain.cpp
+++ src/DedicatedServerMain.cpp
@@ -31,6 +31,8 @@
 #include <World.h>
 #include <nlohmann/json.hpp>
 
+#include "Components/Collider.h"
+#include "Components/Rigidbody.h"
 #include "Magnum/ImGuiIntegration/Context.hpp"
 
 using namespace Magnum;
@@ -120,6 +122,7 @@ namespace Game {
         float _cameraMoveSpeed{0.1f};
 
         std::shared_ptr<World> _world;
+        std::shared_ptr<DedicatedServer> _server;
     };
 
 
@@ -149,58 +152,236 @@ namespace Game {
 
 
     ServerGameApp::ServerGameApp(const Magnum::Platform::GlfwApplication::Arguments& args)
-    : Magnum::Platform::Application{args} {
-        const Vector2 dpiScaling = this->dpiScaling({});
-        Configuration conf;
-        conf.setTitle("Server Game App")
-                .setSize(conf.size(), dpiScaling);
-        GLConfiguration glConf;
-        glConf.setSampleCount(dpiScaling.max() < 2.0f ? 8 : 2);
-        if (!tryCreate(conf, glConf))
-            create(conf, glConf.setSampleCount(0));
+    : Magnum::Platform::Application{args, NoCreate} {
+        {
+            const Vector2 dpiScaling = this->dpiScaling({});
+            Configuration conf;
+            conf.setTitle("Server Game App")
+                    .setSize(conf.size(), dpiScaling);
+            GLConfiguration glConf;
+            glConf.setSampleCount(dpiScaling.max() < 2.0f ? 8 : 2);
+            if (!tryCreate(conf, glConf))
+                create(conf, glConf.setSampleCount(0));
 
-        _imgui = ImGuiIntegration::Context(
-            Vector2{windowSize()}/dpiScaling,
-                windowSize(),
-                framebufferSize());
+            _imgui = ImGuiIntegration::Context(
+                Vector2{windowSize()}/dpiScaling,
+                    windowSize(),
+                    framebufferSize());
 
-        /* Set up proper blending to be used by ImGui. There's a great chance
-           you'll need this exact behavior for the rest of your scene. If not, set
-           this only for the drawFrame() call. */
-        GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add,
-            GL::Renderer::BlendEquation::Add);
-        GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::SourceAlpha,
-            GL::Renderer::BlendFunction::OneMinusSourceAlpha);
+            /* Set up proper blending to be used by ImGui. There's a great chance
+               you'll need this exact behavior for the rest of your scene. If not, set
+               this only for the drawFrame() call. */
+            GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add,
+                GL::Renderer::BlendEquation::Add);
+            GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::SourceAlpha,
+                GL::Renderer::BlendFunction::OneMinusSourceAlpha);
+        }
+
+        _server = std::make_shared<DedicatedServer>(5555, 4);
+
+        _world = std::make_shared<World>(_timeline);
+
+        /* Camera setup */
+        _cameraRig = _world->createGameObject();
+        _cameraRig->translate(Vector3::yAxis(3.0f)).rotateY(40.0_degf);
+
+        _cameraObject = _world->createGameObject(_cameraRig);
+        _cameraObject->translate(Vector3::zAxis(20.0f)).rotateX(-25.0_degf);
+
+        (_camera = new SceneGraph::Camera3D(*_cameraObject))
+                ->setAspectRatioPolicy(SceneGraph::AspectRatioPolicy::Extend)
+                .setProjectionMatrix(Matrix4::perspectiveProjection(35.0_degf, 1.0f, 0.1f, 1000.0f))
+                .setViewport(GL::defaultFramebuffer.viewport().size());
+
+
+        /* Create an instanced shader */
+        _shader = Shaders::PhongGL{
+            Shaders::PhongGL::Configuration{}
+            .setFlags(Shaders::PhongGL::Flag::VertexColor |
+                      Shaders::PhongGL::Flag::InstancedTransformation)
+        };
+        _shader.setAmbientColor(0x111111_rgbf)
+                .setSpecularColor(0x330000_rgbf)
+                .setLightPositions({{10.0f, 15.0f, 5.0f, 0.0f}});
+
+        /* Box and sphere mesh, with an (initially empty) instance buffer */
+        _box = MeshTools::compile(Primitives::cubeSolid());
+        _sphere = MeshTools::compile(Primitives::uvSphereSolid(16, 32));
+        _boxInstanceBuffer = GL::Buffer{};
+        _sphereInstanceBuffer = GL::Buffer{};
+        _box.addVertexBufferInstanced(_boxInstanceBuffer, 1, 0,
+                                      Shaders::PhongGL::TransformationMatrix{},
+                                      Shaders::PhongGL::NormalMatrix{},
+                                      Shaders::PhongGL::Color3{});
+        _sphere.addVertexBufferInstanced(_sphereInstanceBuffer, 1, 0,
+                                         Shaders::PhongGL::TransformationMatrix{},
+                                         Shaders::PhongGL::NormalMatrix{},
+                                         Shaders::PhongGL::Color3{});
+
+        /* Setup the renderer so we can draw the debug lines on top */
+        GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+        GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+        GL::Renderer::enable(GL::Renderer::Feature::PolygonOffsetFill);
+        GL::Renderer::setPolygonOffset(2.0f, 0.5f);
+
+        /* Bullet setup */
+        _world->_debugDraw = BulletIntegration::DebugDraw{};
+        _world->_debugDraw.setMode(BulletIntegration::DebugDraw::Mode::DrawWireframe);
+
+        /* Create the ground */
+        //auto *ground = new RigidBody{&_scene, 0.0f, &_bGroundShape, _bWorld};
+        const std::shared_ptr<GameObject> ground = _world->createGameObject();
+        ground->addComponent<Collider>(reinterpret_cast<btCollisionShape*>(&_bGroundShape));
+        ground->addComponent<Rigidbody>(0.0f);
+
+        new ColoredDrawable{
+            *ground, _boxInstanceData, 0xffffff_rgbf,
+            Matrix4::scaling({100.0f, 0.5f, 100.0f}), _drawables
+        };
+
+        /* Create boxes with random colors */
+        Deg hue = 42.0_degf;
+        for (Int i = 0; i != 10; ++i) {
+            for (Int j = 0; j != 10; ++j) {
+                for (Int k = 0; k != 5; ++k) {
+                    const std::shared_ptr<GameObject> newBox = _world->createGameObject();
+                    auto collider = newBox->addComponent<Collider>(reinterpret_cast<btCollisionShape*>(&_bBoxShape));
+                    const auto rb = newBox->addComponent<Rigidbody>(1.0f);
+                    newBox->translate({i + 1.0f , j + 5.0f, k + 1.0f});
+                    rb->syncPose();
+                    new ColoredDrawable{
+                        *newBox, _boxInstanceData,
+                        Color3::fromHsv({hue += 137.5_degf, 0.75f, 0.9f}),
+                        Matrix4::scaling(Vector3{0.5f}), _drawables
+                    };
+                }
+            }
+        }
+
+        /* Loop at 60 Hz max */
+        setSwapInterval(1);
+        setMinimalLoopPeriod(16.0_msec);
+        _timeline.start();
     }
 
     void ServerGameApp::drawEvent() {
+        GL::defaultFramebuffer.clear(GL::FramebufferClear::Color | GL::FramebufferClear::Depth);
+
+        /* Housekeeping: remove any objects which are far away from the origin */
+        /*
+        for (Object3D *obj = _scene.children().first(); obj;) {
+            Object3D *next = obj->nextSibling();
+            if (obj->transformation().translation().dot() > 100 * 100)
+                delete obj;
+
+            obj = next;
+        }
+        */
+
+        /* Step bullet simulation */
+        _world->update();
+
+        if (_drawCubes) {
+            /* Populate instance data with transformations and colors */
+            arrayResize(_boxInstanceData, 0);
+            arrayResize(_sphereInstanceData, 0);
+
+            /* Draw the objects */
+            _camera->draw(_drawables);
+
+            _shader.setProjectionMatrix(_camera->projectionMatrix());
+
+            /* Upload instance data to the GPU (orphaning the previous buffer
+               contents) and draw all cubes in one call, and all spheres (if any)
+               in another call */
+            _boxInstanceBuffer.setData(_boxInstanceData, GL::BufferUsage::DynamicDraw);
+            _box.setInstanceCount(_boxInstanceData.size());
+            _shader.draw(_box);
+
+            _sphereInstanceBuffer.setData(_sphereInstanceData, GL::BufferUsage::DynamicDraw);
+            _sphere.setInstanceCount(_sphereInstanceData.size());
+            _shader.draw(_sphere);
+        }
+
+        /* Debug draw. If drawing on top of cubes, avoid flickering by setting
+           depth function to <= instead of just <. */
+        if (_drawDebug) {
+            if (_drawCubes)
+                GL::Renderer::setDepthFunction(GL::Renderer::DepthFunction::LessOrEqual);
+
+            _world->_debugDraw.setTransformationProjectionMatrix(
+                _camera->projectionMatrix() * _camera->cameraMatrix());
+            _world->getBulletWorld().debugDrawWorld();
+
+            if (_drawCubes)
+                GL::Renderer::setDepthFunction(GL::Renderer::DepthFunction::Less);
+        }
+
+        _imgui.newFrame();
+        drawUI();
+        _imgui.updateApplicationCursor(*this);
+
+        /* Set appropriate states. If you only draw ImGui, it is sufficient to
+       just enable blending and scissor test in the constructor. */
+        GL::Renderer::enable(GL::Renderer::Feature::Blending);
+        GL::Renderer::enable(GL::Renderer::Feature::ScissorTest);
+        GL::Renderer::disable(GL::Renderer::Feature::FaceCulling);
+        GL::Renderer::disable(GL::Renderer::Feature::DepthTest);
+
+        _imgui.drawFrame();
+
+        /* Reset state. Only needed if you want to draw something else with
+       different state after. */
+        GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+        GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+        GL::Renderer::disable(GL::Renderer::Feature::ScissorTest);
+        GL::Renderer::disable(GL::Renderer::Feature::Blending);
+
+        swapBuffers();
+        _timeline.nextFrame();
+        redraw();
     }
 
     void ServerGameApp::drawUI() const {
     }
 
     void ServerGameApp::keyPressEvent(KeyEvent &event) {
+        if(_imgui.handleKeyPressEvent(event)) return;
     }
 
     void ServerGameApp::keyReleaseEvent(KeyEvent &event) {
+        if(_imgui.handleKeyReleaseEvent(event)) return;
     }
 
     void ServerGameApp::pointerPressEvent(PointerEvent &event) {
+        if(_imgui.handlePointerPressEvent(event)) return;
     }
 
     void ServerGameApp::pointerReleaseEvent(PointerEvent &event) {
+        if (_imgui.handlePointerReleaseEvent(event)) return;
     }
 
     void ServerGameApp::scrollEvent(ScrollEvent &event) {
+        if(_imgui.handleScrollEvent(event)) {
+            /* Prevent scrolling the page */
+            event.setAccepted();
+            return;
+        }
     }
 
     void ServerGameApp::pointerMoveEvent(PointerMoveEvent &event) {
+        if (_imgui.handlePointerMoveEvent(event)) return;
     }
 
     void ServerGameApp::textInputEvent(TextInputEvent &event) {
+        if(_imgui.handleTextInputEvent(event)) return;
     }
 
     void ServerGameApp::viewportEvent(ViewportEvent &event) {
+        GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+
+        _imgui.relayout(Vector2{event.windowSize()}/event.dpiScaling(),
+            event.windowSize(), event.framebufferSize());
     }
 }
 
